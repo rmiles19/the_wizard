c0c2c63031f9ebc56a752c3a51ac08cf934a8c53
Rachel Miles <milesrachel9@gmail.com>
2018-03-21 11:55:00 -0600

    add edit profile form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import NoMatch from './NoMatch';
 import NavBar from './NavBar';
 import Login from './Login';
 import Register from './Register';
 import Flash from './Flash';
 import Home from './Home';
+import Profile from './Profile';
 import ProtectedRoute from './ProtectedRoute';
 import AuthRoute from './AuthRoute';
 import FetchUser from './FetchUser';
-import { Switch, Route } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
@@ -18,7 +20,8 @@ class App extends Component {
         <Flash />
         <FetchUser>
           <Switch>
-            <Route exact path='/' component={Home} />
+            <ProtectedRoute exact path='/' component={Home} />
+            <ProtectedRoute exact path='/profile' component={Profile} />
             <AuthRoute exact path='/login' component={Login} />
             <AuthRoute exact path='/register' component={Register} />
             <Route component={NoMatch} />
